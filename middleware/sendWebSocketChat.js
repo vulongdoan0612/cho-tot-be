e953a9742365452642d23d64967505c4504d9a0f
Vũ Nguyễn <longvuxautrai12@gmail.com>
2024-05-26 22:37:46 +0700

    update

--- middleware/sendWebSocketChat.js
+++ middleware/sendWebSocketChat.js
@@ -2,8 +2,8 @@ import { WebSocket, WebSocketServer } from "ws";
 
 export const webSocketChat = (wss, action, idRoom, status) => {
   const message = JSON.stringify({ action, idRoom, status });
-  console.log(message, client.readyState === WebSocket.OPEN, WebSocket.OPEN, client.readyState);
   wss.clients.forEach(function each(client) {
+    console.log(message, client.readyState === WebSocket.OPEN, WebSocket.OPEN, client.readyState);
     if (client.readyState === WebSocket.OPEN) {
       client.send(message);
     }
