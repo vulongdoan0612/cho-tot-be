8e535071586d24057754f11e3e02157457e2e1c2
nguyendoanthanhlongvu <myemail>
2024-05-28 02:11:36 +0700

    update

--- index.js
+++ index.js
@@ -5,11 +5,9 @@ import cors from "cors";
 import userRouter from "./routes/userRoutes.js";
 import formPostRouter from "./routes/formPostRoutes.js";
 import adminRouter from "./routes/adminCMS.js";
-import { Server } from "http";
 import favPostRouter from "./routes/favPost.js";
 import chatRouter from "./routes/chat.js";
 import paymentRouter from "./routes/payment.js";
-import { WebSocket, WebSocketServer } from "ws";
 
 dotenv.config();
 
@@ -37,29 +35,10 @@ app.use("/", formPostRouter);
 app.use("/", adminRouter);
 app.use("/", favPostRouter);
 app.use("/", paymentRouter);
+app.use("/", chatRouter);
 
-// Create WebSocket server
-// const wss = new WebSocketServer({ port: 80, path: "/ws" });
 
-// wss.on("connection", (ws) => {
-//   console.log("New client connected");
 
-//   ws.on("message", (message) => {
-//     console.log("received: %s", message);
-//   });
-
-//   ws.on("close", () => {
-//     console.log("Client disconnected");
-//   });
-// });
-app.use(
-  "/",
-  (req, res, next) => {
-    req.wss = wss;
-    next();
-  },
-  chatRouter
-);
 const port = 5000;
 
 app.use(cors(corsOptions));
