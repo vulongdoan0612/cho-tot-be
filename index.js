5542ae298c30260e04991ac46f8825500bfd70dd
vulongdoan0612 <myemail>
2024-05-28 14:28:09 +0700

    update

--- index.js
+++ index.js
@@ -31,38 +31,38 @@ const corsOptions = {
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
 };
-const wss = new WebSocketServer({ port: 443, path: "/ws" });
+// const wss = new WebSocketServer({ port: 443, path: "/ws" });
 
-wss.on("connection", (ws) => {
-  console.log("New WebSocket client connected");
+// wss.on("connection", (ws) => {
+//   console.log("New WebSocket client connected");
 
-  ws.on("message", (message) => {
-    console.log("Received:", message);
-    // Xử lý message từ client
-  });
+//   ws.on("message", (message) => {
+//     console.log("Received:", message);
+//     // Xử lý message từ client
+//   });
 
-  ws.on("close", () => {
-    console.log("WebSocket client disconnected");
-  });
-});
+//   ws.on("close", () => {
+//     console.log("WebSocket client disconnected");
+//   });
+// });
 
-export const webSocketChat = (action, idRoom) => {
-  const message = JSON.stringify({ action, idRoom });
-  wss.clients.forEach((client) => {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(message);
-    }
-  });
-};
-export const sendAnnouce = (action, userId, announce) => {
-  const message = JSON.stringify({ action, userId, announce });
+// export const webSocketChat = (action, idRoom) => {
+//   const message = JSON.stringify({ action, idRoom });
+//   wss.clients.forEach((client) => {
+//     if (client.readyState === WebSocket.OPEN) {
+//       client.send(message);
+//     }
+//   });
+// };
+// export const sendAnnouce = (action, userId, announce) => {
+//   const message = JSON.stringify({ action, userId, announce });
 
-  wss.clients.forEach(function each(client) {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(message);
-    }
-  });
-};
+//   wss.clients.forEach(function each(client) {
+//     if (client.readyState === WebSocket.OPEN) {
+//       client.send(message);
+//     }
+//   });
+// };
 app.use("/", userRouter);
 app.use("/", formPostRouter);
 app.use("/", adminRouter);
