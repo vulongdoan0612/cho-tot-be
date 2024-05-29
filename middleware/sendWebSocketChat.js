cf1d0a62e7894d670dcec3b8b96350ecef51b45f
vulongdoan0612 <myemail>
2024-05-29 17:20:05 +0700

    update

--- middleware/sendWebSocketChat.js
+++ middleware/sendWebSocketChat.js
@@ -3,9 +3,17 @@ import { WebSocket } from "ws";
 export const webSocketChat = (wss, action, idRoom) => {
   const messageText = JSON.stringify({ action, idRoom });
   // Broadcast the message to all clients
-  wss.clients.forEach(function each(client) {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(messageText);
-    }
+  wss.on("connection", (ws, request) => {
+    ws.on("message", (message) => {
+      console.log(`Received message: ${message}`);
+      // Broadcast the message to all clients
+      wss.clients.forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(messageText);
+        }
+      });
+    });
+
+    ws.send("Welcome to the WebSocket server!");
   });
 };
