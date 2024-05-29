eb40995d11775cf209e4ea8a8293fb2fd7df1c1c
vulongdoan0612 <myemail>
2024-05-29 17:27:24 +0700

    update

--- index.js
+++ index.js
@@ -38,19 +38,19 @@ port8085.on("upgrade", (request, socket, head) => {
     socket.destroy();
   }
 });
-// wss8085.on("connection", (ws, request) => {
-//   ws.on("message", (message) => {
-//     console.log(`Received message: ${message}`);
-//     // Broadcast the message to all clients
-//     wss8085.clients.forEach((client) => {
-//       if (client.readyState === WebSocket.OPEN) {
-//         client.send(message);
-//       }
-//     });
-//   });
+wss8085.on("connection", (ws, request) => {
+  ws.on("message", (message) => {
+    console.log(`Received message: ${message}`);
+    // Broadcast the message to all clients
+    wss8085.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
 
-//   ws.send("Welcome to the WebSocket server!");
-// });
+  ws.send("Welcome to the WebSocket server!");
+});
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
